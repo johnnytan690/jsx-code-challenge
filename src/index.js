26e39b3f0395e5a0f0700fb46173c1fc834d4028
johnnytan690 <94107329+johnnytan690@users.noreply.github.com>
2022-05-26 10:33:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My favourite food</h1>
+    <ul>
+      <li>Sushi</li>
+      <li>Egg</li>
+      <li>Fried rice</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
